b844e04d5b0f8df58bb202ffef1362c340b559e7
steRn1412 <stern.bin7@gmail.com>
2024-07-01 15:46:25 +0800

    Responsive

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -10,11 +10,11 @@ import { SubsCardsComponents } from "@/src/components/home/SubsCardsComponents";
 import { HomeNavbar } from "@/src/components/home/navigation/HomeNavbar";
 import { HomeFooter } from "@/src/components/home/navigation/HomeFooter";
 import { useTranslations } from "next-intl";
-import CrossImg from '../../public/assets/home/Section2.png'
-import Frame3 from '../../public/assets/home/Frame3.png'
-import hero from '../../public/assets/home/Banner.png'
-import NextLink from 'next/link'
-import { Link } from '@/src/navigation'
+import CrossImg from "../../public/assets/home/Section2.png";
+import Frame3 from "../../public/assets/home/Frame3.png";
+import hero from "../../public/assets/home/Banner.png";
+import NextLink from "next/link";
+import { Link } from "@/src/navigation";
 // import { CardsComponents } from "@/components/home/CardsComponents";
 // import { ListCardComponents } from "@/components/home/ListCardComponents";
 // import { WhyCardsComponents } from "@/components/home/WhyCardsComponents";
@@ -23,59 +23,59 @@ import { Link } from '@/src/navigation'
 // import { HomeNavbar } from "@/components/home/navigation/HomeNavbar";
 // import { HomeFooter } from "@/components/home/navigation/HomeFooter";
 export default function Home() {
-
   const t = useTranslations("default");
-  const signup = 'https://client.shiplink.ca/auth/signup'
+  const signup = "https://client.shiplink.ca/auth/signup";
 
   return (
     <>
       <div className={styles.container}>
-      <div class=" relative h-screen flex flex-col items-center justify-center text-center text-white ">
-      <div class="flex absolute inset-0">
-              <Image
-                alt="mailbox"
-                src={hero}
-                styles={"min-w-full min-h-full absolute object-cover"}
-              />
-      <div class="absolute inset-0 bg-black opacity-30"></div>
-      </div>
-      <div class="relative z-10 flex flex-col justify-center items-center h-full text-center">
-    <h1 class="text-5xl font-bold leading-tight mb-4">{t('Header')}</h1>
-    <p class="text-3xl font-bold leading-tight mb-4">{t('SubHeader')}</p>
-    <NextLink passHref href={'#hiw'} >
+        <div class=" relative h-screen flex flex-col items-center justify-center text-center text-white ">
+          <div class="flex absolute inset-0">
+            <Image
+              alt="mailbox"
+              src={hero}
+              styles={"min-w-full min-h-full absolute object-cover"}
+            />
+            <div class="absolute inset-0 bg-black opacity-30"></div>
+          </div>
+          <div class="relative z-10 flex flex-col justify-center items-center h-full text-center">
+            <h1 class="text-5xl font-bold leading-tight mb-4">{t("Header")}</h1>
+            <p class="text-3xl font-bold leading-tight mb-4">
+              {t("SubHeader")}
+            </p>
+            <NextLink passHref href={"#hiw"}>
               <Button
                 variant="destructive"
                 className="rounded w-[182px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
               >
-                <p className="text-lg">{t('Learn')}</p>
+                <p className="text-lg">{t("Learn")}</p>
               </Button>
-              </NextLink>
-  </div>
-
-
+            </NextLink>
+          </div>
         </div>
-   
 
         {/* section */}
         <div className="w-[100%] bg-white">
           <div className={`flex flex-col gap-10 w-[90%] mx-auto my-10 `}>
             <div className="flex flex-col justify-center gap-5">
-              <h1 className=" text-myBlue text-lg font-bold">{t('MembershipHeader')}</h1>
-              <h1 className=" text-black text-3xl font-bold">{t('2ways')}</h1>
+              <h1 className=" text-myBlue text-lg font-bold">
+                {t("MembershipHeader")}
+              </h1>
+              <h1 className=" text-black text-3xl font-bold">{t("2ways")}</h1>
               <div className="text-[#5A5A5A] text-base">
                 <ul>
-                  <li>
-                    - {t('2WaysLS')}
-                  </li>
-                  <li>
-                    - {t('2WaysLSTwo')}
-                  </li>
+                  <li>- {t("2WaysLS")}</li>
+                  <li>- {t("2WaysLSTwo")}</li>
                 </ul>
               </div>
               <div className="py-5 mt-3 flex flex-col gap-5 justify-center items-center">
                 <SubsCardsComponents />
-                <Button variant="secondary" size="lg" className="rounded px-10 mt-10 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-                  <p className="text-base">{t('PaP')}</p>
+                <Button
+                  variant="secondary"
+                  size="lg"
+                  className="rounded px-10 mt-10 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                >
+                  <p className="text-base">{t("PaP")}</p>
                 </Button>
               </div>
             </div>
@@ -83,16 +83,21 @@ export default function Home() {
         </div>
 
         {/* section */}
-        <div id="hiw" className={`${styles.works} py-20 gap-10 bg-gradient-to-br from-blue-50 to-white`} >
+        <div
+          id="hiw"
+          className={`${styles.works} py-20 gap-10 bg-gradient-to-br from-blue-50 to-white`}
+        >
           <div className="flex flex-row gap-5 justify-between items-center mx-auto w-[90%] flex-wrap">
             <div className="left flex flex-col justify-start gap-5 lg:w-[50%]">
               <h2 className="text-4xl text-myBlue font-bold">
-                {t('CrossBorder')}
+                {t("CrossBorder")}
               </h2>
-              <h1 className=" text-black text-lg font-bold">{t('CrossBorder-works')}</h1>
+              <h1 className=" text-black text-lg font-bold">
+                {t("CrossBorder-works")}
+              </h1>
               <div className="text-[#5A5A5A] text-base">
                 <p className="w-[90%] leading-loose">
-                  {t('CrossBorder-params')}
+                  {t("CrossBorder-params")}
                 </p>
               </div>
             </div>
@@ -108,39 +113,38 @@ export default function Home() {
             </div>
           </div>
           <div className="flex flex-col mt-10 gap-5 mx-auto w-[90%]">
-            <p className="text-xl font-bold">{t('Buy')}</p>
+            <p className="text-xl font-bold">{t("Buy")}</p>
             <div className="cardContent">
               <CardsComponents />
             </div>
           </div>
         </div>
 
-
         {/* section */}
         <div className={`${styles.sectionFree}`}>
           <div className={`${styles.contentFrame} mx-auto`}>
             <div className="flex flex-col gap-5 justify-center items-center text-center  px-10 py-10 h-[100%]">
-              <h3 className="text-3xl  font-bold text-white">{t('Account')}</h3>
-              <p className="text-base px-4 text-white">{t('Account-param')}</p>
-              <NextLink passHref href={`${signup}`} >
-              <Button
-                variant="destructive"
-                size="lg"
-                className="rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-              >
-                <p className="text-base ">{t('Account-get')}</p>
-              </Button>
+              <h3 className="text-3xl  font-bold text-white">{t("Account")}</h3>
+              <p className="text-base px-4 text-white">{t("Account-param")}</p>
+              <NextLink passHref href={`${signup}`}>
+                <Button
+                  variant="destructive"
+                  size="lg"
+                  className="rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                >
+                  <p className="text-base ">{t("Account-get")}</p>
+                </Button>
               </NextLink>
             </div>
           </div>
         </div>
 
         {/* section */}
-        <div className={`${styles.works} py-20 gap-10 bg-gradient-to-br from-blue-50 to-white`} >
+        <div
+          className={`${styles.works} py-20 gap-10 bg-gradient-to-br from-blue-50 to-white`}
+        >
           <div className="w-[90%] ">
-            <h2 className="text-4xl text-myBlue font-bold">
-              {t('Shipping')}
-            </h2>
+            <h2 className="text-4xl text-myBlue font-bold">{t("Shipping")}</h2>
           </div>
           <div className="flex flex-col gap-8 mx-auto w-[90%] lg:flex-row">
             <div className="left w-[300px] h-max flex flex-col justify-start gap-5 lg:w-[600px]">
@@ -155,42 +159,48 @@ export default function Home() {
 
                 <div className="p-5 w-[280px] absolute bottom-10 left-10 rounded shadow flex flex-col gap-[20px] bg-white md:w-[390px]">
                   <h3 className="text-xl  font-bold text-black">Lorem Ipsum</h3>
-                  <p className="text-base text-[#5A5A5A]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempora accusamus iste in cum expedita sint optio ipsam laudantium quaerat.</p>
+                  <p className="text-base text-[#5A5A5A]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Odit tempora accusamus iste in cum expedita sint optio ipsam
+                    laudantium quaerat.
+                  </p>
                 </div>
               </div>
             </div>
 
             <div className="right w-full flex flex-col justify-between h-[100%] gap-6">
-              <h3 className="text-xl font-bold text-black">{t('worksHead')}</h3>
+              <h3 className="text-xl font-bold text-black">{t("worksHead")}</h3>
               <div className="h-full">
                 <ListCardComponents />
               </div>
             </div>
           </div>
           <Link passHref href={"/shippingLabels"}>
-          <Button
-            variant="destructive"
-            size="lg"
-            className="rounded px-10 w-[182px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-          >
-            <p className="text-base">{t('MD')}</p>
-          </Button>
+            <Button
+              variant="destructive"
+              size="lg"
+              className="rounded px-10 w-[182px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+            >
+              <p className="text-base">{t("MD")}</p>
+            </Button>
           </Link>
 
           <div className={`${styles.contentFrame2} mx-auto my-[20px]`}>
             <div className="flex flex-col gap-5 justify-center items-center text-center px-10 py-16 h-[100%]">
               <div className="py-5 gap-4 flex flex-col">
-                <h3 className="text-3xl text-center font-bold text-white">{t('Signup')}</h3>
-                <p className="text-base px-4 text-white">{t('SignupP')}</p>
+                <h3 className="text-3xl text-center font-bold text-white">
+                  {t("Signup")}
+                </h3>
+                <p className="text-base px-4 text-white">{t("SignupP")}</p>
               </div>
-              <NextLink passHref href={`${signup}`} >
-              <Button
-                variant="destructive"
-                size="lg"
-                className="rounded px-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-              >
-                <p className="text-base">{t('GMA')}</p>
-              </Button>
+              <NextLink passHref href={`${signup}`}>
+                <Button
+                  variant="destructive"
+                  size="lg"
+                  className="rounded px-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                >
+                  <p className="text-base">{t("GMA")}</p>
+                </Button>
               </NextLink>
             </div>
           </div>
@@ -199,9 +209,7 @@ export default function Home() {
         {/* Section Why */}
         <div className="w-[100%] bg-white">
           <div className={`flex flex-col gap-10 w-[90%] mx-auto my-10 `}>
-            <h2 className="text-4xl text-myBlue font-bold">
-              {t('whyHead')}
-            </h2>
+            <h2 className="text-4xl text-myBlue font-bold">{t("whyHead")}</h2>
             <div className="bg-gradient-to-r from-blue-900 to-blue-900/90 w-full rounded-sm p-[32px]">
               <WhyCardsComponents />
             </div>
@@ -210,19 +218,19 @@ export default function Home() {
 
         {/* Patner Section */}
         <div className="w[100%] bg-[#F7F7F7] border border-[#E7E8EC] py-10">
-          <div className={`flex flex-col gap-10 w-[90%] mx-auto justify-center items-center my-10 `}>
+          <div
+            className={`flex flex-col gap-10 w-[90%] mx-auto justify-center items-center my-10 `}
+          >
             <div className="flex flex-col justify-center w-full gap-5 text-center">
-              <h2 className="text-4xl text-black font-bold">
-              {t('OWC')}
-              </h2>
-              <p>{t('WoW')}</p>
+              <h2 className="text-4xl text-black font-bold">{t("OWC")}</h2>
+              <p>{t("WoW")}</p>
               <div className="py-10 mt-10 w-full">
                 <CarrierList />
               </div>
             </div>
           </div>
         </div>
-      </div >
+      </div>
     </>
   );
 }

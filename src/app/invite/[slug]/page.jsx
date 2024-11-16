c1bf10b9b7265d62ff253298da2dd3ac12dffc3a
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-11-16 19:46:27 +0700

    Chore : Update InvitePage redirect to use environment variable for signup URL

--- src/app/invite/[slug]/page.jsx
+++ src/app/invite/[slug]/page.jsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation';
 
 export default function InvitePage({ params }) {
     const { slug } = params;
+    const baseRedirect = process.env.NEXT_PUBLIC_SIGNUP_URL;
     // Redirect ke halaman signup
-    redirect(`https://client.shiplink.ca/auth/signup/${slug}`);
+    redirect(`${baseRedirect}/${slug}`);
 }

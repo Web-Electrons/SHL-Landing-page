550796157680f1eaff14396adf17591cd3cbb436
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-11-18 17:10:22 +0700

    Chore : Testing With Hardcode redirect URL

--- src/app/invite/[slug]/page.jsx
+++ src/app/invite/[slug]/page.jsx
@@ -2,11 +2,13 @@
 import { redirect } from 'next/navigation';
 
 export async function generateMetadata({ params }) {
-    const baseRedirect = process.env.NEXT_PUBLIC_SIGNUP_URL;
+    // const baseRedirect = process.env.NEXT_PUBLIC_SIGNUP_URL;
+    const baseRedirect = 'https://client.shiplink.ca/es/auth/signup';
+
     const { slug } = params;
 
     // Redirect server-side
-    redirect(`${baseRedirect}/${slug}`);
+    redirect(`https://client.shiplink.ca/en/auth/signup/${slug}`);
 }
 
 export default function InvitePage() {

5796e27f92be6c39cf674899bab983cdd7a7f19c
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-11-18 15:07:12 +0700

    Chore : Update Meta Images

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -8,13 +8,13 @@ import { cn } from '@/lib/utils'
 import { Toaster } from "@/src/components/ui/toaster"
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
+import images from '../../public/images/shipLink_preview.png'
 export const poppins = Poppins({
     subsets: ['latin'],
     weight: ['100', '300', '400', '500', '600', '700', '900'],
     variable: '--font-poppins',
     display: 'swap',
     tabluar: true,
-
 })
 export const roboto = Roboto({
     subsets: ['latin'],
@@ -34,7 +34,7 @@ export const metadata = {
         locale: 'en_US', // Pastikan locale sesuai dengan target audiens
         images: [
             {
-                url: 'https://w3.shiplink.ca/images/shipLink_preview.png',
+                url: images.src,
                 width: 1200,
                 height: 630,
                 alt: 'ShipLink package forwarding preview',
@@ -45,7 +45,7 @@ export const metadata = {
         card: 'summary_large_image',
         title: 'ShipLink - Best Package Forwarding Service',
         description: "ShipLink is the world's best package forwarding service, offering worldwide coverage and secure shipping.",
-        images: ['https://w3.shiplink.ca/images/shipLink_preview.png'], // URL absolut untuk gambar
+        images: [images.src], // URL absolut untuk gambar
     },
     facebook: {
         appId: '1234567890', // Tambahkan ID aplikasi Facebook jika Anda memilikinya

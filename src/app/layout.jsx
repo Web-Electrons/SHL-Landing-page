adca8774f91d940bc4ac36b1d7a086c99156a35f
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-11-18 15:34:45 +0700

    Chore : Update Open Graph and Twitter meta tags with correct URLs and image paths

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,19 +18,19 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
 
         {/* Facebook Meta Tags */}
-        <meta property="og:url" content="https://w3.shiplink.ca/en" />
+        <meta property="og:url" content="https://www.shiplink.com/en" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="ShipLink - Best Package Forwarding Service" />
         <meta property="og:description" content="ShipLink is the world's best package forwarding service, offering worldwide coverage and secure shipping." />
-        <meta property="og:image" content="http://localhost:3000/_next/static/media/shipLink_preview.841c909a.png"></meta>
+        <meta property="og:image" content="https://www.shiplink.com/_next/static/media/shipLink_preview.841c909a.png"></meta>
 
         {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="w3.shiplink.ca" />
-        <meta property="twitter:url" content="https://w3.shiplink.ca/en" />
+        <meta property="twitter:url" content="https://www.shiplink.com/en" />
         <meta name="twitter:title" content="ShipLink - Best Package Forwarding Service" />
         <meta name="twitter:description" content="ShipLink is the world's best package forwarding service, offering worldwide coverage and secure shipping." />
-        <meta name="twitter:image" content="http://localhost:3000/_next/static/media/shipLink_preview.841c909a.png" />
+        <meta name="twitter:image" content="https://www.shiplink.com/_next/static/media/shipLink_preview.841c909a.png" />
       </head>
       <body className={inter.className}>
         {children}

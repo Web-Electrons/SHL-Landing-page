57f34fc1c42a83de9fb302fbd3f9f373f407edf1
Rangga Alroy <114912822+RanggaAlroy@users.noreply.github.com>
2025-11-25 20:01:47 +0700

    fix error handling

--- src/app/[locale]/shipping_calculator/components/panel/CourrierCard.jsx
+++ src/app/[locale]/shipping_calculator/components/panel/CourrierCard.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import { formatCurrency, formatDecimal } from '@/lib/utils';
 import React from 'react'
 
 export const CourrierCard = ({ data, isSelected, onSelect }) => {
@@ -10,16 +11,16 @@ export const CourrierCard = ({ data, isSelected, onSelect }) => {
       <div className="flex flex-row justify-between items-center">
         <div className="flex flex-row gap-2 items-center">
           <img
-            src={data.providerImage200 || '/placeholder-logo.png'}
-            alt={data.provider}
+            src={data?.providerImage200 || '/placeholder-logo.png'}
+            alt={data?.provider}
             className="h-8 w-8 object-center object-contain p-1 bg-blue-100 rounded"
           />
           <div className="flex flex-col w-[70%]">
-            <p className='text-xs'>{data.provider} - {data.servicelevel?.name}</p>
+            <p className='text-xs'>{data?.provider} - {data?.servicelevel?.name}</p>
             <p className="text-[10px] text-gray-500">Estimate {data.estimatedDays || "-"} Days</p>
           </div>
         </div>
-        <p className='text-xs w-[30%] text-right'>{`${data.currency} ${data.amountLocal.toFixed(2)}`}</p>
+        <p className='text-xs w-[30%] text-right'>{`${formatCurrency(data?.currency)} ${formatDecimal(data?.amountLocal || 0)}`}</p>
       </div>
     </div>
   );

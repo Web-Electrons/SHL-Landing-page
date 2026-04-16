0b4f6b7effabda96808e4bd6f30d262005f17e4f
sahrilputra <iam.sahrilputra@gmail.com>
2026-04-16 11:21:55 +0700

    Feat : adding typescript, tenstack query, husky, vitest, eslint, update vs env, and adding prettier formatter

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -1,83 +1,81 @@
-import React from "react"
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 // import { HomeNavbar } from "@/src/components/home/navigation/HomeNavbar"
-import { HomeNavbar } from "@/components/home/navigation/HomeNavbar"
-import { HomeFooter } from "@/components/home/navigation/HomeFooter"
-import { Roboto, Poppins } from 'next/font/google'
-import './globals.scss'
-import { cn } from '@/lib/utils'
-import { Toaster } from "@/components/ui/toaster"
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
-import images from '../../public/images/shipLink_preview.png'
-import smallImages from '../../public/images/favicon.ico'
+import { HomeFooter } from "@/components/home/navigation/HomeFooter";
+import { HomeNavbar } from "@/components/home/navigation/HomeNavbar";
+import { Toaster } from "@/components/ui/toaster";
+import { cn } from "@/lib/utils";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { Poppins, Roboto } from "next/font/google";
+import TenstackProvider from "../../../utils/provider/tenstackQuery.provider";
+import smallImages from "../../public/images/favicon.ico";
+import images from "../../public/images/shipLink_preview.png";
+import "./globals.scss";
 export const poppins = Poppins({
-    subsets: ['latin'],
-    weight: ['100', '300', '400', '500', '600', '700', '900'],
-    variable: '--font-poppins',
-    display: 'swap',
-    tabluar: true,
-})
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "600", "700", "900"],
+  variable: "--font-poppins",
+  display: "swap",
+  tabluar: true,
+});
 export const roboto = Roboto({
-    subsets: ['latin'],
-    weight: ['100', '300', '400', '500', '700', '900'],
-    variable: '--font-roboto',
-    display: 'swap',
-})
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--font-roboto",
+  display: "swap",
+});
 export const metadata = {
-    title: 'ShipLink - Best Package Forwarding Service',
-    description: "Worldwide best package forwarding service.",
-    openGraph: {
-        title: 'ShipLink - Best Package Forwarding Service',
-        description: "ShipLink is the world's best package forwarding service, offering worldwide coverage and secure shipping.",
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}`, // URL absolut dari halaman ini
-        type: 'website',
-        siteName: 'ShipLink', // Menambahkan nama website
-        locale: 'en_US', // Pastikan locale sesuai dengan target audiens
-        images: [
-            {
-                url: images.src,
-                width: 1200,
-                height: 630,
-                alt: 'ShipLink package forwarding preview',
-            },
-        ],
-    },
-    twitter: {
-        card: 'summary_large_image',
-        title: 'ShipLink - Best Package Forwarding Service',
-        description: "ShipLink is the world's best package forwarding service, offering worldwide coverage and secure shipping.",
-        images: [images.src], // URL absolut untuk gambar
-    },
-    facebook: {
-        appId: '1234567890', // Tambahkan ID aplikasi Facebook jika Anda memilikinya
-    },
-    whatsapp: {
-        title: 'ShipLink - Best Package Forwarding Service',
-        description: "ShipLink is the world's best package forwarding service, offering worldwide coverage and secure shipping.",
-        image: smallImages.src, // WhatsApp will show the image link too
-    },
+  title: "ShipLink - Best Package Forwarding Service",
+  description: "Worldwide best package forwarding service.",
+  openGraph: {
+    title: "ShipLink - Best Package Forwarding Service",
+    description:
+      "ShipLink is the world's best package forwarding service, offering worldwide coverage and secure shipping.",
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}`, // URL absolut dari halaman ini
+    type: "website",
+    siteName: "ShipLink", // Menambahkan nama website
+    locale: "en_US", // Pastikan locale sesuai dengan target audiens
+    images: [
+      {
+        url: images.src,
+        width: 1200,
+        height: 630,
+        alt: "ShipLink package forwarding preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ShipLink - Best Package Forwarding Service",
+    description:
+      "ShipLink is the world's best package forwarding service, offering worldwide coverage and secure shipping.",
+    images: [images.src], // URL absolut untuk gambar
+  },
+  facebook: {
+    appId: "1234567890", // Tambahkan ID aplikasi Facebook jika Anda memilikinya
+  },
+  whatsapp: {
+    title: "ShipLink - Best Package Forwarding Service",
+    description:
+      "ShipLink is the world's best package forwarding service, offering worldwide coverage and secure shipping.",
+    image: smallImages.src, // WhatsApp will show the image link too
+  },
 };
 
 export default function RootLayout({ children, params: { locale } }) {
+  const messages = getMessages({ locale });
 
-    const messages = getMessages({ locale });
-
-    return (
-        <NextIntlClientProvider locale={locale} messages={messages}>
-            <main
-                className={cn(
-                    "min-h-screen font-sans antialiased",
-                    roboto.variable
-                )}
-            >
-                <div className={styles.mains}>
-                    <HomeNavbar />
-                    {children}
-                    <HomeFooter />
-                </div>
-            </main>
-            <Toaster />
-        </NextIntlClientProvider>
-    )
+  return (
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <TenstackProvider />
+      <main className={cn("min-h-screen font-sans antialiased", roboto.variable)}>
+        <div className={styles.mains}>
+          <HomeNavbar />
+          {children}
+          <HomeFooter />
+        </div>
+      </main>
+      <Toaster />
+    </NextIntlClientProvider>
+  );
 }
--- src/app/[locale]/shipping_calculator/components/panel/CourrierCard.jsx
+++ src/app/[locale]/shipping_calculator/components/panel/CourrierCard.jsx
@@ -1,26 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
-import { formatCurrency, formatDecimal } from '@/lib/utils';
-import React from 'react'
+import { formatCurrency, formatDecimal } from "@/lib/utils";
 
 export const CourrierCard = ({ data, isSelected, onSelect }) => {
   return (
     <div
-      className={`card border border-gray-300 p-2 rounded cursor-pointer ${isSelected ? 'bg-red-100' : 'hover:bg-red-100'}`}
+      className={`card cursor-pointer rounded border border-gray-300 p-2 ${isSelected ? "bg-red-100" : "hover:bg-red-100"}`}
       onClick={() => onSelect(data)}
     >
-      <div className="flex flex-row justify-between items-center">
-        <div className="flex flex-row gap-2 items-center">
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row items-center gap-2">
           <img
-            src={data?.providerImage200 || '/placeholder-logo.png'}
+            src={data?.providerImage200 || "/placeholder-logo.png"}
             alt={data?.provider}
-            className="h-8 w-8 object-center object-contain p-1 bg-blue-100 rounded"
+            className="h-8 w-8 rounded bg-blue-100 object-contain object-center p-1"
           />
-          <div className="flex flex-col w-[70%]">
-            <p className='text-xs'>{data?.provider} - {data?.servicelevel?.name}</p>
+          <div className="flex w-[70%] flex-col">
+            <p className="text-xs">
+              {data?.provider} - {data?.servicelevel?.name}
+            </p>
             <p className="text-[10px] text-gray-500">Estimate {data.estimatedDays || "-"} Days</p>
           </div>
         </div>
-        <p className='text-xs w-[30%] text-right'>{`${formatCurrency(data?.currency)} ${formatDecimal(data?.amountLocal || 0)}`}</p>
+        <p className="w-[30%] text-right text-xs">{`${formatCurrency(data?.currency)} ${formatDecimal(data?.amountLocal || 0)}`}</p>
       </div>
     </div>
   );
